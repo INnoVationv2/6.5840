26d1a6a444318ca9411d4f0d3c6c3b81dcbc7988
Liu,Yu <code.liuyu@gmail.com>
2024-09-20 22:13:34 +0800

    TestSpeed4A Passed

--- src/kvraft/getter_and_setter.go
+++ src/kvraft/getter_and_setter.go
@@ -5,3 +5,27 @@ import "sync/atomic"
 func (kv *KVServer) getAppliedLogIdx() int32 {
 	return atomic.LoadInt32(&kv.appliedLogIdx)
 }
+
+func (kv *KVServer) getTerm() int32 {
+	return atomic.LoadInt32(&kv.raftTerm)
+}
+
+func (kv *KVServer) setTerm(term int32) {
+	atomic.StoreInt32(&kv.raftTerm, term)
+}
+
+func (kv *KVServer) getRole() int32 {
+	return atomic.LoadInt32(&kv.role)
+}
+
+func (kv *KVServer) isLeader() bool {
+	return atomic.LoadInt32(&kv.role) == LEADER
+}
+
+func (kv *KVServer) turnToLeader() {
+	atomic.StoreInt32(&kv.role, LEADER)
+}
+
+func (kv *KVServer) turnToFollower() {
+	atomic.StoreInt32(&kv.role, FOLLOWER)
+}
