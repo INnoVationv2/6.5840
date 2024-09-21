dae064c862d2b4fbe743813b58b4ffea54944edf
Liu,Yu <code.liuyu@gmail.com>
2024-09-21 13:11:28 +0800

    Bug Fix

--- src/kvraft/util.go
+++ src/kvraft/util.go
@@ -1,5 +1,7 @@
 package kvraft
 
+import "fmt"
+
 func min(x, y int32) int32 {
 	if x <= y {
 		return x
@@ -12,4 +14,23 @@ func max(x, y int32) int32 {
 		return x
 	}
 	return y
+}
+
+func compareCommand(cmd1 *Command, cmd2 *Command) bool {
+	cmp := cmd1.Type == cmd2.Type && cmd1.Key == cmd2.Key
+	if cmd1.Type != GET {
+		cmp = cmp && cmd1.Value == cmd2.Value
+	}
+	return cmp
+}
+
+func (kv *KVServer) getRoleStr() string {
+	if kv.isLeader() {
+		return "Leader"
+	}
+	return "Follower"
+}
+
+func (kv *KVServer) getServerDetail() string {
+	return fmt.Sprintf("%s %d", kv.getRoleStr(), kv.me)
 }
